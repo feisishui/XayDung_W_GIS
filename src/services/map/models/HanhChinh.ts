8726656916aa40f976efca94b70562951dc9708d
fU-Hi <fuhi.dev@gmail.com>
2019-04-09 17:13:10 +0700

    sap xep hanh chinh

--- src/services/map/models/HanhChinh.ts
+++ src/services/map/models/HanhChinh.ts
@@ -7,6 +7,26 @@ export default interface HanhChinh {
 
 export const values: HanhChinh[] = [
   {
+    TenHuyenTP: 'TP. Thủ Dầu Một',
+    MaHuyenTP: '718'
+  },
+  {
+    TenHuyenTP: 'TX. Dĩ An',
+    MaHuyenTP: '724'
+  },
+  {
+    TenHuyenTP: 'TX. Thuận An',
+    MaHuyenTP: '725'
+  },
+  {
+    TenHuyenTP: 'TX. Tân Uyên',
+    MaHuyenTP: '723'
+  },
+  {
+    TenHuyenTP: 'TX. Bến Cát',
+    MaHuyenTP: '721'
+  },
+  {
     TenHuyenTP: 'H. Bàu Bàng',
     MaHuyenTP: '726'
   },
@@ -15,31 +35,11 @@ export const values: HanhChinh[] = [
     MaHuyenTP: '720'
   },
   {
-    TenHuyenTP: 'TX. Dĩ An',
-    MaHuyenTP: '724'
-  },
-  {
     TenHuyenTP: 'H. Phú Giáo',
     MaHuyenTP: '722'
   },
   {
     TenHuyenTP: 'H. Bắc Tân Uyên',
     MaHuyenTP: '727'
-  },
-  {
-    TenHuyenTP: 'TP. Thủ Dầu Một',
-    MaHuyenTP: '718'
-  },
-  {
-    TenHuyenTP: 'TX. Thuận An',
-    MaHuyenTP: '725'
-  },
-  {
-    TenHuyenTP: 'H. Tân Uyên',
-    MaHuyenTP: '723'
-  },
-  {
-    TenHuyenTP: 'H. Bến Cát',
-    MaHuyenTP: '721'
   }
 ]
